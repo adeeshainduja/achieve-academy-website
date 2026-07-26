22a462c81b8f2441693a5a0fdff6dce8a1a4b006
Adeesha Induja <adeeshainduja@gmail.com>
2026-07-26 23:30:17 +0530

    change about to about section and edit

--- achieve-academy-frontend/src/App.tsx
+++ achieve-academy-frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero/Hero";
+import AboutSection from "./components/sections/AboutSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <AboutSection />
     </div>
   );
 }
