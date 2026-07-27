25a3d13399609bfcf21f8fdcac62f17ceb40f654
Adeesha Induja <adeeshainduja@gmail.com>
2026-07-27 07:38:54 +0530

    concted chatbot api

---
+++ achieve-academy-frontend/src/api/chatbotApi.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { ChatRequest, ChatResponse } from "../types/chatbot";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+});
+
+export const sendMessage = async (
+  data: ChatRequest
+): Promise<ChatResponse> => {
+  const response = await api.post("/api/chat/", data);
+  return response.data;
+};
