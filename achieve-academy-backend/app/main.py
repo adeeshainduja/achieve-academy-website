58025450b28270eaff29f2ba9f39cfa6ed506835
Adeesha Induja <adeeshainduja@gmail.com>
2026-07-27 08:13:43 +0530

    fixed CORS error in backend

--- achieve-academy-backend/app/main.py
+++ achieve-academy-backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.chatbot import router as chatbot_router
 
 app = FastAPI(
     title="Achieve Academy AI Assistant",
     version="1.0.0"
+)
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(chatbot_router)
