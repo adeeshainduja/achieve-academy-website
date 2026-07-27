4583b9dc7253c95ec8bc9025d7162e9be3c489ed
Adeesha Induja <adeeshainduja@gmail.com>
2026-07-27 16:23:09 +0530

    add ground in 3d animation

--- achieve-academy-frontend/src/components/sections/Hero/Ground.tsx
+++ achieve-academy-frontend/src/components/sections/Hero/Ground.tsx
@@ -4,21 +4,19 @@ export default function Ground() {
   return (
     <mesh
       rotation={[-Math.PI / 2, 0, 0]}
-      position={[0, -1.45, 0]}
+      position={[0, -1, 0]}
       receiveShadow
     >
       <planeGeometry args={[30, 30]} />
 
       <MeshReflectorMaterial
-        blur={[300, 100]}
         resolution={1024}
+        blur={[300, 100]}
         mixBlur={1}
-        mixStrength={20}
-        roughness={1}
-        depthScale={1}
-        minDepthThreshold={0.85}
-        color="#dff7e5"
-        metalness={0.2}
+        mixStrength={8}
+        roughness={0.85}
+        color="#fafafa"
+        metalness={0}
       />
     </mesh>
   );
