f3e672e1ebce3c1d456d96f051997a6a1d02df04
Adeesha Induja <adeeshainduja@gmail.com>
2026-07-27 07:16:38 +0530

    connect to api

--- achieve-academy-backend/app/main.py
+++ achieve-academy-backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
+from app.routers.chatbot import router as chatbot_router
+
 app = FastAPI(
     title="Achieve Academy AI Assistant",
     version="1.0.0"
 )
+
+app.include_router(chatbot_router)
+
 
 @app.get("/")
 def home():
