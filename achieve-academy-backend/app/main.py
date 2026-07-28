3e23c7dcad0fb6f361d9c16da92972d7ec30d296
Adeesha Induja <adeeshainduja@gmail.com>
2026-07-28 05:57:31 +0530

    change main file for allow frontend url

--- achieve-academy-backend/app/main.py
+++ achieve-academy-backend/app/main.py
@@ -8,13 +8,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# Allowed Frontend URLs
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://achieve-academy-website.vercel.app",
+]
+
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
